5fcc2d64abefddf6a1877ecf0f42be942dc23969
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2019-09-11 22:54:17 -0300

    Estrutura inicial da API de Marketplace

---
+++ README.md
@@ -0,0 +1,7 @@
+## Docker Mongo
+
+** sudo docker run --name mongonode -p 27017:27017 -d mongo **
+
+## Docker Redis
+
+** sudo docker run --name noderedis -p 6379:6379 -d -t redis:alpine **
---
+++ src/app/controllers/PurchaseController.js
@@ -0,0 +1,25 @@
+const Ad = require('../models/Ad');
+const User = require('../models/User');
+const Mail = require('../services/Mail');
+const Queue = require('../services/Queue');
+const jobs = require('../jobs');
+
+class PurchaseController {
+  async store (req, res) {
+    const { ad, content } = req.body;
+
+    const purchaseAd = await Ad.findById(ad).populate('author');
+    const user = await User.findById(req.userId);
+
+    Queue.create(jobs.PurchaseMail.key, {
+      ad: purchaseAd,
+      user,
+      content
+    }).save();
+
+    return res.send();
+
+  }
+}
+
+module.exports = new PurchaseController();
